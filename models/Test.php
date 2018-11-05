1367baa9df950627ed579896cb39971b7072e96e
System <system@ciast.gov.my>
2018-11-05 14:26:49 +0800

    Init

---
+++ models/Test.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $age
+ * @property string $date
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'age'], 'required'],
+            [['age'], 'integer'],
+            [['date'], 'safe'],
+            [['name'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'age' => 'Age',
+            'date' => 'Date',
+        ];
+    }
+}
